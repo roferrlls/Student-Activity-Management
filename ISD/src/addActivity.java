92e08d0f2a600f8acbb83c95c2efeb6b808b89e5
Tirth Shah <roferrlls@gmail.com>
2018-12-10 20:52:46 +0530

    Add files via upload

--- ISD/src/addActivity.java
+++ ISD/src/addActivity.java
@@ -1,4 +1,5 @@
 import java.awt.EventQueue;
+import java.util.Date;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,25 +13,28 @@ import java.sql.Statement;
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JTextPane;
+import javax.swing.SpinnerDateModel;
 import javax.swing.JTextField;
 import javax.swing.JEditorPane;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JComboBox;
+import com.toedter.calendar.JDayChooser;
+import com.toedter.calendar.JDateChooser;
+import javax.swing.JSpinner;
+import java.awt.Color;
+import javax.swing.UIManager;
 
 public class addActivity {
 
 	public JFrame frame;
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
 	   static final String DB_URL = "jdbc:mysql://localhost/s";
-	   public int id =0;
 
 	   //  Database credentials
-	   static final String USER = "kritika";
-	   static final String PASS = "lnmiit";
+	   static final String USER = "root";
+	   static final String PASS = "root";
 	   Connection conn = null;
 	   PreparedStatement stmt = null;
-	   Statement stmt1 = null;
 
 	/**
 	 * Launch the application.
@@ -60,12 +64,17 @@ public class addActivity {
 	 */
 	private void initialize() {
 		frame = new JFrame("Add Activity");
+		frame.getContentPane().setFont(new Font("Dialog", Font.PLAIN, 14));
+		frame.getContentPane().setBackground(new Color(255, 250, 205));
 		frame.setBounds(100, 100, 800, 800);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JEditorPane editorPane = new JEditorPane();
-		editorPane.setBounds(310, 63, 349, 28);
+		editorPane.setFont(new Font("Dialog", Font.PLAIN, 14));
+		editorPane.setForeground(new Color(0, 0, 0));
+		editorPane.setBackground(Color.WHITE);
+		editorPane.setBounds(310, 64, 349, 28);
 		frame.getContentPane().add(editorPane);
 		
 		JLabel lblName = new JLabel("NAME");
@@ -74,15 +83,16 @@ public class addActivity {
 		lblName.setBounds(47, 76, 70, 15);
 		frame.getContentPane().add(lblName);
 		
-		JLabel lblDate = new JLabel("DATE(yyyy - mm - dd)");
+		JLabel lblDate = new JLabel("DATE");
 		lblDate.setFont(new Font("Serif", Font.PLAIN, 14));
 
 		lblDate.setBounds(48, 122, 208, 28);
 		frame.getContentPane().add(lblDate);
 		
-		JEditorPane editorPane_1 = new JEditorPane();
-		editorPane_1.setBounds(310, 129, 181, 21);
-		frame.getContentPane().add(editorPane_1);
+		JDateChooser dateChooser = new JDateChooser();
+		dateChooser.setBackground(new Color(0, 128, 0));
+		dateChooser.setBounds(314, 122, 192, 28);
+		frame.getContentPane().add(dateChooser);
 		
 		JLabel lblDateyyyytimeDd = new JLabel("TIME(hh:mm::ss)");
 		lblDateyyyytimeDd.setToolTipText("");
@@ -90,9 +100,16 @@ public class addActivity {
 		lblDateyyyytimeDd.setBounds(48, 188, 208, 28);
 		frame.getContentPane().add(lblDateyyyytimeDd);
 		
-		JEditorPane editorPane_2 = new JEditorPane();
-		editorPane_2.setBounds(310, 184, 181, 21);
-		frame.getContentPane().add(editorPane_2);
+		
+		
+		JSpinner timeSpinner = new JSpinner( new SpinnerDateModel() );
+		timeSpinner.setBackground(Color.LIGHT_GRAY);
+		JSpinner.DateEditor timeEditor = new JSpinner.DateEditor(timeSpinner, "HH:mm:ss");
+		timeSpinner.setEditor(timeEditor);
+		timeSpinner.setValue(new Date());
+		timeSpinner.setBounds(310, 180, 196, 28);
+		frame.getContentPane().add(timeSpinner);
+		
 		
 		JLabel lblVenue = new JLabel("VENUE");
 		lblVenue.setFont(new Font("Serif", Font.PLAIN, 14));
@@ -100,6 +117,8 @@ public class addActivity {
 		frame.getContentPane().add(lblVenue);
 		
 		JEditorPane editorPane_3 = new JEditorPane();
+		editorPane_3.setFont(new Font("Dialog", Font.PLAIN, 14));
+		editorPane_3.setBackground(Color.WHITE);
 		editorPane_3.setBounds(310, 235, 349, 28);
 		frame.getContentPane().add(editorPane_3);
 		
@@ -109,6 +128,8 @@ public class addActivity {
 		frame.getContentPane().add(lblExpectedAudience);
 		
 		JEditorPane editorPane_4 = new JEditorPane();
+		editorPane_4.setFont(new Font("Dialog", Font.PLAIN, 14));
+		editorPane_4.setBackground(Color.WHITE);
 		editorPane_4.setBounds(310, 296, 181, 21);
 		frame.getContentPane().add(editorPane_4);
 		
@@ -118,6 +139,8 @@ public class addActivity {
 		frame.getContentPane().add(lblContactDetails);
 		
 		JEditorPane editorPane_5 = new JEditorPane();
+		editorPane_5.setFont(new Font("Dialog", Font.PLAIN, 14));
+		editorPane_5.setBackground(Color.WHITE);
 		editorPane_5.setBounds(310, 345, 181, 21);
 		frame.getContentPane().add(editorPane_5);
 		
@@ -127,10 +150,14 @@ public class addActivity {
 		frame.getContentPane().add(lblDescription);
 		
 		JEditorPane editorPane_6 = new JEditorPane();
-		editorPane_6.setBounds(310, 407, 459, 43);
+		editorPane_6.setFont(new Font("Dialog", Font.PLAIN, 14));
+		editorPane_6.setForeground(new Color(0, 0, 0));
+		editorPane_6.setBackground(Color.WHITE);
+		editorPane_6.setBounds(310, 407, 459, 76);
 		frame.getContentPane().add(editorPane_6);
 		
 		JButton btnAddJudge = new JButton("ADD judge");
+		btnAddJudge.setBackground(new Color(135, 206, 235));
 		btnAddJudge.setBounds(284, 732, 117, 25);
 		frame.getContentPane().add(btnAddJudge);
 		
@@ -140,11 +167,13 @@ public class addActivity {
 		frame.getContentPane().add(comboBox);
 		
 		JLabel lblNewLabel = new JLabel("CLUB");
+		lblNewLabel.setFont(new Font("Dialog", Font.PLAIN, 14));
 		lblNewLabel.setBounds(61, 500, 70, 15);
 		frame.getContentPane().add(lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel("Department");
+		JLabel lblNewLabel_1 = new JLabel("DEPARTMENT");
 		lblNewLabel_1.setBounds(61, 570, 117, 15);
+		lblNewLabel_1.setFont(new Font("Dialog", Font.PLAIN, 14));
 		frame.getContentPane().add(lblNewLabel_1);
 		
 		String[] dept = {"CSE","ECE","CCE"};
@@ -153,51 +182,60 @@ public class addActivity {
 		frame.getContentPane().add(comboBox_1);
 		
 		JButton Submit = new JButton("SUBMIT");
+		Submit.setBackground(new Color(135, 206, 235));
 		Submit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				String name = editorPane.getText().toString();
-				String date =  editorPane_1.getText().toString();
-				String time = editorPane_2.getText().toString();
+				
+				// getting date
+				String whole = dateChooser.getDate().toString();				
+				String date = whole.substring(24,28);
+				String m = null;
+				date = date + "-";
+				String month = whole.substring(4,7);
+				String[] tp = new String[12];
+				tp[0] = "Jan";
+				tp[1] = "Feb";
+				tp[2] = "Mar";
+				tp[3] = "Aprr";
+				tp[4] = "May";
+				tp[5] = "Jun";
+				tp[6] = "Jul";
+				tp[7] = "Aug";
+				tp[8] = "Sep";
+				tp[9] = "Oct";
+				tp[10] = "Nov";
+				tp[11] = "Dec";
+				for(int i=0;i<12;i++) {
+					if(tp[i].equals(month)) {
+						m = Integer.toString(i+1);
+					}
+				}
+				if(m.length() == 1) {
+					m = "0" + m;
+				}
+				date = date + m;
+				String day = whole.substring(8,10);
+				date = date + "-" + day;
+				//System.out.println(date);
+				
+				
+				//getting time
+				
+				String val=timeSpinner.getValue().toString();
+				String time=val.substring(11,19);
+				
+				
+				
+				
+				//String date =  editorPane_1.getText().toString();
+				//String time = editorPane_2.getText().toString();
 				String venue = editorPane_3.getText().toString();
 				String exaud = editorPane_4.getText().toString();
 				String contact = editorPane_5.getText().toString();
 				String description = editorPane_6.getText().toString();
 				String club = String.valueOf(comboBox.getItemAt(comboBox.getSelectedIndex()));
 				String dept = String.valueOf(comboBox_1.getItemAt(comboBox_1.getSelectedIndex()));
-				if(name.length() == 0) {
-					JOptionPane.showMessageDialog(frame,"Name field is empty");  
-					return;
-				}
-				if(date.length() == 0) {
-					JOptionPane.showMessageDialog(frame,"date field is empty");  
-					return;
-				}
-				if(time.length() == 0) {
-					JOptionPane.showMessageDialog(frame,"time field is empty");  
-					return;
-				}
-				if(venue.length() == 0) {
-					JOptionPane.showMessageDialog(frame,"venue field is empty");  
-					return;
-				}
-				if(exaud.length() == 0) {
-					JOptionPane.showMessageDialog(frame,"expected Audience field is empty");  
-					return;
-				}
-				if(contact.length() == 0) {
-					JOptionPane.showMessageDialog(frame,"contact field is empty");  
-					return;
-				}
-				if(description.length() == 0) {
-					JOptionPane.showMessageDialog(frame,"description field is empty");  
-					return;
-				}
-				if(club.length() == 0) {
-					club = "";
-				}
-				if(dept.length() == 0) {
-					dept="";  
-				}
 				//System.out.print(club + " "+ dept + " " + time + " " + venue + " " + exaud + " " + contact);
 				try{
 				      //STEP 2: Register JDBC driver
@@ -209,17 +247,8 @@ public class addActivity {
 
 				      //STEP 4: Execute a query
 				      System.out.println("Creating statement...");
-				      String s= "select Aid from Activity";
-				      stmt1 = conn.createStatement();
-				      ResultSet rs = stmt1.executeQuery(s);
-				      while(rs.next()) {
-				    	  id++;
-				      }
-				      
-				      System.out.println(id);
 				      String query = "insert into Activity (name,time,date,venue,description,Expected_Audience,Contact_Details,Club,Department)" + " values(?,?,?,?,?,?,?,?,?)";
 				      stmt = conn.prepareStatement(query);
-				      
 				      stmt.setString(1, name);
 				      stmt.setString(2, time);
 				      stmt.setString(3, date);
@@ -242,11 +271,8 @@ public class addActivity {
 				   }finally{
 				      //finally block used to close resources
 				      try{
-				         if(stmt!=null) {
+				         if(stmt!=null)
 				            stmt.close();
-				            stmt1.close();
-				         }   
-				         	
 				      }catch(SQLException se2){
 				      }// nothing we can do
 				      try{
@@ -257,25 +283,11 @@ public class addActivity {
 				      }//end finally try
 				   }
 				
-				EventQueue.invokeLater(new Runnable() {
-						public void run() {
-						try {
-							Features window = new Features(id);
-							window.frame.setVisible(true);
-							
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-					}
-				});
-				
 			}
 		});
 		Submit.setBounds(269, 677, 145, 43);
 		frame.getContentPane().add(Submit);
-		
-		
-		
+			
 		
 	}
 }
