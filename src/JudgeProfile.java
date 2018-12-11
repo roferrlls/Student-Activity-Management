7bcc317015a875c567c7206c58ed42a47434eff4
kritika0598 <30694412+kritika0598@users.noreply.github.com>
2018-12-11 18:55:50 +0530

    Add files via upload

---
+++ src/JudgeProfile.java
@@ -0,0 +1,174 @@
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JList;
+
+public class JudgeProfile {
+
+	public JFrame frame;
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	   static final String DB_URL = "jdbc:mysql://localhost/s";
+
+	   //  Database credentials
+	   static final String USER = "root";
+	   static final String PASS = "root";
+	   Connection conn = null;
+	   PreparedStatement stmt = null;
+	   PreparedStatement stmt1=null;
+	   public String email;
+	   
+	   
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		
+		
+	}
+
+	/**
+	 * Create the application.
+	 * @wbp.parser.entryPoint
+	 */
+	public JudgeProfile(String email) {
+		this.email=email;
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame("JudgeProfile");
+		frame.setBounds(100, 100, 800, 800);
+		DefaultListModel<String> l1 = new DefaultListModel<>();   
+        try{
+		      //STEP 2: Register JDBC driver
+		      Class.forName("com.mysql.jdbc.Driver");
+
+		      //STEP 3: Open a connection
+		      System.out.println("Connecting to database...");
+		      conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+		      //STEP 4: Execute a query
+		      System.out.println("Creating statement...");
+		      String sql;
+		      sql = "select Aid from Contains where Judge_Email= " + "?";
+		      stmt = conn.prepareStatement(sql);
+		      stmt.setString(1,email);
+		      ResultSet rs = stmt.executeQuery();
+		      while(rs.next()){
+			         //Retrieve by column name
+			        
+			        int id = rs.getInt("Aid");
+			         System.out.println( id);
+			         try{
+					     
+					      String query;
+					      query = "select * from Activity where Aid = " + "?";
+					      stmt1 = conn.prepareStatement(query);
+					      stmt1.setInt(1,id);
+					      ResultSet rs1 = stmt1.executeQuery();
+					      
+					      while(rs1.next()){
+						         //Retrieve by column name
+						        String name = rs1.getString("Name");
+						        String str = "(" + id + ")" ;
+						        str += " "  +name;
+						         l1.addElement(str);
+						        
+						       
+						      }
+					      
+					      
+					   }catch(SQLException se){
+					      //Handle errors for JDBC
+					      se.printStackTrace();
+					   }catch(Exception e){
+					      //Handle errors for Class.forName
+					      e.printStackTrace();
+					   }
+			         
+			       
+			      }
+		      rs.close();
+		      stmt.close();
+		      conn.close();
+		   }catch(SQLException se){
+		      //Handle errors for JDBC
+		      se.printStackTrace();
+		   }catch(Exception e){
+		      //Handle errors for Class.forName
+		      e.printStackTrace();
+		   }finally{
+		      //finally block used to close resources
+		      try{
+		         if(stmt!=null)
+		            stmt.close();
+		      }catch(SQLException se2){
+		      }// nothing we can do
+		      try{
+		         if(conn!=null)
+		            conn.close();
+		      }catch(SQLException se){
+		         se.printStackTrace();
+		      }//end finally try
+		   }
+        JList<String> list = new JList<>(l1);  
+        list.setBounds(12,34, 721,510);  
+        frame.getContentPane().add(list);  
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnSubmitMarks = new JButton("VIEW PARTICIPANTS");
+		btnSubmitMarks.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				EventQueue.invokeLater(new Runnable() {
+					public void run() {
+						try {
+							String dest = list.getSelectedValue();
+							int i =1;
+							String temp = "";
+							temp += dest.charAt(i);
+							while(dest.charAt(i+1) != ')') {
+								temp += dest.charAt(i+1);
+								i++;
+							}
+							int val = Integer.parseInt(temp);
+							AssignMarks window = new AssignMarks(val,email);
+							window.frame.setVisible(true);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
+			}
+		});	
+		btnSubmitMarks.setBounds(209, 591, 395, 25);
+		frame.getContentPane().add(btnSubmitMarks);
+		
+		JButton btnNewButton = new JButton("Back");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				frame.dispose();
+			}
+		});	
+		btnNewButton.setBounds(209, 685, 395, 25);
+		frame.getContentPane().add(btnNewButton);
+		
+		
+		
+	}
+
+}
---
+++ src/Login.java
@@ -0,0 +1,144 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JEditorPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.awt.event.ActionEvent;
+
+public class Login {
+
+	public JFrame frame;
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	   static final String DB_URL = "jdbc:mysql://localhost/s";
+	   static final String USER = "kritika";
+	   static final String PASS = "lnmiit";
+	   Connection conn = null;
+	   Statement stmt = null;
+	   PreparedStatement stmt1 = null;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login window = new Login();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Login() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblRollno = new JLabel("Roll_No");
+		lblRollno.setBounds(37, 30, 70, 15);
+		frame.getContentPane().add(lblRollno);
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setBounds(37, 96, 70, 15);
+		frame.getContentPane().add(lblPassword);
+		
+		JEditorPane editorPane = new JEditorPane();
+		editorPane.setBounds(201, 24, 106, 21);
+		frame.getContentPane().add(editorPane);
+		
+		JEditorPane editorPane_1 = new JEditorPane();
+		editorPane_1.setBounds(201, 90, 106, 21);
+		frame.getContentPane().add(editorPane_1);
+		
+		JButton btnSubmit = new JButton("LOGIN");
+		btnSubmit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String roll = editorPane.getText().toString();
+				String password = editorPane_1.getText().toString();
+				String rpassword = "";
+				try{
+				      //STEP 2: Register JDBC driver
+				      Class.forName("com.mysql.jdbc.Driver");
+
+				      //STEP 3: Open a connection
+				      System.out.println("Connecting to database...");
+				      conn = DriverManager.getConnection(DB_URL,USER,PASS);
+				      stmt = conn.createStatement();
+
+				      //STEP 4: Execute a query
+				      System.out.println("Creating statement...");
+				      String sql;
+				      sql = "select Password from Participant where Pid = '" + roll+"'";
+				      ResultSet rs = stmt.executeQuery(sql);
+				      while(rs.next()) {
+				    	 rpassword = rs.getString(1);
+				    		  
+				      }
+				      stmt.close();
+				      conn.close();
+				   }catch(SQLException se){
+				      //Handle errors for JDBC
+				      se.printStackTrace();
+				   }catch(Exception ee){
+				      //Handle errors for Class.forName
+				      ee.printStackTrace();
+				   }finally{
+				      //finally block used to close resources
+				      try{
+				         if(stmt!=null)
+				            stmt.close();
+				      }catch(SQLException se2){
+				      }// nothing we can do
+				      try{
+				         if(conn!=null)
+				            conn.close();
+				      }catch(SQLException se){
+				         se.printStackTrace();
+				      }//end finally try
+				   }
+				if(password.equals(rpassword)) {
+					EventQueue.invokeLater(new Runnable() {
+						public void run() {
+							try {
+								MyAccount window = new MyAccount(roll);
+								window.frame.setVisible(true);
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
+					});
+				}
+				else {
+					JOptionPane.showMessageDialog(frame,"Invalid UserName or password");  
+					return;
+				}
+				
+			}
+		});
+		btnSubmit.setBounds(120, 173, 117, 25);
+		frame.getContentPane().add(btnSubmit);
+	}
+}
